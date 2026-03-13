775b5a799797ab0f6c7f5e1d5a32cf0bdaa6d451
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2026-03-13 11:29:47 +0100

    init setup

---
+++ src/main/java/com/neon/releasetracker/ReleasetrackerApplication.java
@@ -0,0 +1,12 @@
+package com.neon.releasetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasetrackerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReleasetrackerApplication.class, args);
+  }
+}
